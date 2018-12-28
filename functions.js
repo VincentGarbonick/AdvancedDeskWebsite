4a4361468dd687c79b16381155b1bd8436f843ef
Vincent Garbonick <vincent.garbonick@autobag.com>
2018-12-28 15:32:08 -0500

    Add info to about page

--- functions.js
+++ functions.js
@@ -1,19 +1,12 @@
-function pageChangeBongo(){
-	location.replace("/extrapages/page1.html");
-}
-
-function pageChangeIdeas(){
-	location.replace("/extrapages/page2.html")
-}
 
 function githubLink(){
 	location.replace("https://github.com/VincentGarbonick")
 }
 
-function pageChangeResume(){
-	location.replace("/extrapages/resume.html")
+function navigate(URL){
+	location.replace(URL);
 }
 
-function pageChangeMain(){
-	location.replace("/main.html");
+function returnMain(){
+	location.replace('/main.html')
 }
