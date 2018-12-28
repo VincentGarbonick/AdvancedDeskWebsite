c2f5f5cf16ac71560d1db2e2c8f933315e66ffcb
Vincent Garbonick <vincent.garbonick@autobag.com>
2018-12-28 11:29:43 -0500

    Add base website

---
+++ functions.js
@@ -0,0 +1,15 @@
+function popupFunction(){
+	window.alert("I am a popup!");
+}
+
+function pageChangeBongo(){
+	location.replace("/extrapages/page1.html");
+}
+
+function pageChangeIdeas(){
+	location.replace("/extrapages/page2.html")
+}
+
+
+
+
