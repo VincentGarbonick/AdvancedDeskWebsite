98c3a19f0fc100c55855f02ed32fd2d0087c22cd
Vincent Garbonick <vincent.garbonick@autobag.com>
2019-01-02 13:53:24 -0500

    add custom roll input validation

--- webApps/diceroller.js
+++ webApps/diceroller.js
@@ -1,22 +1,43 @@
+//checks to see if passed parameter is a number, a spinoff of a jquery function
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+
+
+
+
 function dXRoll(arg1){
+	//picks random variable from one to passed value 
 	result = Math.floor(Math.random() * arg1) + 1;
+	
+	//puts result in the area where "Resultdice" tag is in element
 	document.getElementById("resultDice").innerHTML = result
 	console.log(result);
 }
 
 function dMulti(){
-	var person = prompt("Please enter your name", "Harry Potter");
+	var number = prompt("How many dice are you rolling?", "10");
+	var size = prompt("How big are the dice you are rolling?", "6");
 
-	if (person != null) {
- 	 	document.getElementById("result").innerHTML =
-  		"Hello " + person + "! How are you today?";
-	}
-	else{
-		var test = prompt("suck my meat", "Burger")
-	}
 }
 
 function dCustom(){
 	var size = prompt("How big is the dice you are rolling?", "100");
-	dXRoll(size)
+	
+	if(Math.floor(size) == size && isNumeric(size) && size > 0){
+
+		console.log("valid");
+		dXRoll(size);
+
+	}
+
+	else{
+
+		console.log("NaN");
+		document.getElementById("resultDice").innerHTML = "invalid input";
+		alert("invalid input");
+
+	}
+
 }
